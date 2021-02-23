c56fe08ac37688c7f6c67d08918d014e2377031e
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2021-02-23 10:23:51 -0700

    testing hosting

---
+++ test.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>host me</title>
+    </head>
+    <body style="background-color: blue;">
+        <h3 style="text-align: center;">This may actually work</h3>
+    </body>
+</html>
