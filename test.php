604f0bb6469c420ffc577e89f24bec2df2c3ebd1
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2021-02-23 11:06:04 -0700

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
     <head>
         <title>host me</title>
     </head>
-    <body style="background-color: green;">
+    <body style="background-color: blue;">
         <h3 style="text-align: center;">This may actually work</h3>
     </body>
 </html>
